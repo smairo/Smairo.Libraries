b84bd1f18d7d5bf6b6aab7f854499e7d4a7786e7
Miro Holopainen <smairo@outlook.com>
2020-10-04 14:04:05 +0300

    feat: add function debugger (#5)
    * feat: add function debugger
    
    * chore: clean launchSettings

---
+++ src/Smairo.DependencyContainer/Azure/FunctionDebuggerHost.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+namespace Smairo.DependencyContainer.Azure
+{
+    /// <summary>
+    /// Wrapper for <see cref="IServiceCollection"/> that enables function debugging from another assembly
+    /// </summary>
+    internal class FunctionDebuggerHost : IFunctionsHostBuilder
+    {
+        /// <summary>
+        /// <see cref="IServiceCollection"/>
+        /// </summary>
+        public IServiceCollection Services { get; }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public FunctionDebuggerHost()
+        {
+            Services = new ServiceCollection();
+        }
+    }
+}
